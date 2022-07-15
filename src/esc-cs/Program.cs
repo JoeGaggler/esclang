1d5791550d5ca04ab1117191dc85c9ddbe1d08ce
Joe Gallagher <joegaggler@gmail.com>
2022-07-14 21:50:11 -0500

    read esc source file

--- src/esc-cs/Program.cs
+++ src/esc-cs/Program.cs
@@ -1,1 +1,80 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using static System.Console;
+
+namespace EscLang;
+
+class Program
+{
+	static int Main(string[] args)
+	{
+		var measurements = new List<(String, String)>();
+		var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+		if (args.Length <= 0 || args[0] is not String filePath)
+		{
+			WriteLine("Must provide source file as the parameter.");
+			return 1;
+		}
+
+		if (args.Length <= 1 || args[1] is not String outputPath)
+		{
+			outputPath = Environment.CurrentDirectory;
+		}
+		WriteLine(outputPath);
+
+		String escSourceCode;
+		try
+		{
+			escSourceCode = File.ReadAllText(filePath);
+		}
+		catch
+		{
+			WriteLine($"Unable to read source from path: {filePath}");
+			return 1;
+		}
+		Measure("Read", measurements, stopwatch);
+
+		// TODO: everything else
+
+		// Compiler not yet viable, so currently saving debug information as the output
+		var outputFilePath = Path.Combine(outputPath, "output.txt");
+		try
+		{
+			using (var outputFile = new StreamWriter(outputFilePath))
+			{
+				outputFile.WriteLine("Stats:");
+				Stats(outputFile, measurements);
+			}
+		}
+		catch
+		{
+			WriteLine($"Unable to write output to path: {outputPath}");
+			return 1;
+		}
+
+		Stats(Console.Out, measurements);
+		return 0;
+	}
+
+	private static void Measure(String name, List<(String, String)> measurements, System.Diagnostics.Stopwatch stopwatch)
+	{
+		var time = stopwatch.Elapsed;
+		var nameString = name + ":";
+		var timeString = time.ToString("s\\.ff");
+		measurements.Add((nameString, timeString));
+		stopwatch.Restart();
+	}
+
+	private static void Stats(TextWriter textWriter, List<(String, String)> measurements)
+	{
+		var width1 = measurements.Select(i => i.Item1.Length).Max();
+		var width2 = measurements.Select(i => i.Item2.Length).Max();
+		foreach (var item in measurements)
+		{
+			textWriter.WriteLine($"- {item.Item1.PadLeft(width1)} {item.Item2.PadLeft(width2)}");
+		}
+	}
+}
