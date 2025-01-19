3dc738c7843d223d80f9023c67b50332406649d2
Joe Gallagher <joe@pingmint.net>
2025-01-19 14:22:26 -0500

    test: func-params-2

--- test/Test.cs
+++ test/Test.cs
@@ -1,11 +1,10 @@
 ﻿using static System.Console;
 
-WriteLine(Environment.CurrentDirectory);
-
 Run("cases/first.esc", NoResult());
 Run("cases/first-print-return.esc", Text("hello, world"));
 Run("cases/simple-reassignment.esc", Number(4));
 Run("cases/func-params.esc", Number(13));
+Run("cases/func-params-2.esc", Number(25));
 Run("cases/two-func-calls.esc", Number(999));
 Run("cases/two-func-calls-reverse-declaration.esc", Number(999)); // TODO: out of order declarations
 
